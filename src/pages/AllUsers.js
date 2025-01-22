fd6b5c59c605ce0c448c8bcc772bac3ebe549e06
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-01-23 01:50:27 +0530

    Show all users on UI.

--- src/pages/AllUsers.js
+++ src/pages/AllUsers.js
@@ -4,11 +4,39 @@ const AllUsers = () => {
 
     const {data, isLoading, isError, error} = useGetAllUsersQuery();
 
+    if (isLoading) return <p>Loading...</p>
+    
+    if (isError) {
+        console.log(error);
+        return <p>An Error has occured in loading the data!</p>
+    };
+
     return (
         <>
-            {isLoading && <p>Loading...</p> }
-            {data && <p>Data recieved.</p>}
-            {isError && <p>Error ${error} recieved.</p>}
+            {data && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>E-mail</th>
+                            <th>Company</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((user) => <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.username}</td>
+                            <td>{user.name.split(" ")[0]}</td>
+                            <td>{user.name.split(" ")[1]}</td>
+                            <td>{user.email}</td>
+                            <td>{user.company.name}</td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            }
         </>
     )
 };
