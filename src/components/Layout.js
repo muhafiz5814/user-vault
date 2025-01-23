40916e5792d3a3b2e3ca8f0d5a9c23f15c748f6c
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-01-23 10:59:37 +0530

    make logo a link to home page.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 // Outer layout component for all other page components.
 // This is parent component of every other component.
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 const Layout = () => {
     return (
@@ -8,7 +8,7 @@ const Layout = () => {
             <div className="min-h-screen bg-slate-800 ">
                 <header className="bg-slate-500 text-white">
                     <div>
-                        <p className="text-2xl ms-6 py-2">UserVault</p>
+                        <p className="text-2xl ms-6 py-2"><Link to={"/"}>UserVault</Link></p>
                     </div>
                 </header>
                 <main className="w-full">
